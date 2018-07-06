85f9c742e576cedc8bc610b3852cd9bf40c2aeeb
agawecki <andreas.gaweckicoremedia.com>
2018-07-06 14:16:13 +0200

    some replacements of Guava FluentIterable w/ Java 8 Streams

--- headless-server-lib/src/main/java/com/coremedia/caas/service/repository/ProxyFactoryImpl.java
+++ headless-server-lib/src/main/java/com/coremedia/caas/service/repository/ProxyFactoryImpl.java
@@ -5,29 +5,26 @@ import com.coremedia.caas.service.repository.content.ContentProxyImpl;
 import com.coremedia.cap.content.Content;
 import com.coremedia.cap.content.ContentRepository;
 import com.coremedia.cap.struct.Struct;
-
-import com.google.common.base.Predicates;
-import com.google.common.collect.FluentIterable;
 import com.google.common.collect.Maps;
 
+import javax.validation.constraints.NotNull;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.SortedMap;
-import javax.validation.constraints.NotNull;
+import java.util.stream.Collectors;
 
 public class ProxyFactoryImpl implements ProxyFactory {
 
   private ContentRepository contentRepository;
   private RootContext rootContext;
 
-
-  public ProxyFactoryImpl(ContentRepository contentRepository, RootContext rootContext) {
+  ProxyFactoryImpl(ContentRepository contentRepository, RootContext rootContext) {
     this.contentRepository = contentRepository;
     this.rootContext = rootContext;
   }
-
 
   @Override
   public RootContext getRootContext() {
@@ -44,7 +41,6 @@ public class ProxyFactoryImpl implements ProxyFactory {
       throw new IllegalArgumentException("Invalid root source " + source.getClass().getName());
     }
   }
-
 
   @Override
   public Object makeProxy(Object source) {
@@ -69,7 +65,6 @@ public class ProxyFactoryImpl implements ProxyFactory {
     return source;
   }
 
-
   @Override
   public ContentProxy makeContentProxy(@NotNull Content source) {
     if (rootContext.getAccessControl().check(source)) {
@@ -89,27 +84,37 @@ public class ProxyFactoryImpl implements ProxyFactory {
 
   @Override
   public List<ContentProxy> makeContentProxyList(@NotNull Collection<Content> source) {
-    return FluentIterable.from(source).transform(this::makeContentProxy).filter(Predicates.notNull()).toList();
+    return makeContentProxyList(source, Integer.MAX_VALUE);
   }
 
   @Override
   public List<ContentProxy> makeContentProxyList(@NotNull Collection<Content> source, int limit) {
-    return FluentIterable.from(source).transform(this::makeContentProxy).filter(Predicates.notNull()).limit(limit).toList();
+    return source.stream()
+            .map(this::makeContentProxy)
+            .filter(Objects::nonNull)
+            .limit(limit)
+            .collect(Collectors.toList());
   }
 
   @Override
   public List<ContentProxy> makeContentProxyListFromIds(@NotNull Collection<String> ids) {
-    return FluentIterable.from(ids).transform(this::makeContentProxyFromId).filter(Predicates.notNull()).toList();
+    return makeContentProxyListFromIds(ids, Integer.MAX_VALUE);
   }
 
   @Override
   public List<ContentProxy> makeContentProxyListFromIds(@NotNull Collection<String> ids, int limit) {
-    return FluentIterable.from(ids).transform(this::makeContentProxyFromId).filter(Predicates.notNull()).limit(limit).toList();
+    return ids.stream()
+            .map(this::makeContentProxyFromId)
+            .filter(Objects::nonNull)
+            .limit(limit)
+            .collect(Collectors.toList());
   }
 
-
   private List<Object> makeList(Collection<?> source) {
-    return FluentIterable.from(source).transform(this::makeProxy).filter(Predicates.notNull()).toList();
+    return source.stream()
+            .map(this::makeProxy)
+            .filter(Objects::nonNull)
+            .collect(Collectors.toList());
   }
 
   private Map<?, ?> makeMap(Map<?, ?> source) {
