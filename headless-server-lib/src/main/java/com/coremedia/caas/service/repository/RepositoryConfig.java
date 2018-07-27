b1f61fb66cf1c1cda7c413dcb847df0e3bed1b07
mweikard <mweikard@coremedia.com>
2018-07-27 10:54:59 +0200

    Root context doesn't need to be a spring bean

--- headless-server-lib/src/main/java/com/coremedia/caas/service/repository/RepositoryConfig.java
+++ headless-server-lib/src/main/java/com/coremedia/caas/service/repository/RepositoryConfig.java
@@ -3,11 +3,10 @@ package com.coremedia.caas.service.repository;
 import com.coremedia.blueprint.base.settings.SettingsService;
 import com.coremedia.caas.service.ServiceConfig;
 import com.coremedia.caas.service.repository.content.ContentProxyModelAccessor;
-import com.coremedia.caas.service.request.RequestContext;
 import com.coremedia.cap.content.ContentRepository;
-import com.coremedia.cap.multisite.Site;
 
 import com.google.common.collect.ImmutableList;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,16 +26,9 @@ public class RepositoryConfig {
     return new ProxyFactoryImpl(contentRepository, rootContext);
   }
 
-  @Bean("rootContext")
-  @Scope(BeanDefinition.SCOPE_PROTOTYPE)
-  public RootContext rootContext(Site site, Object currentContext, Object target, RequestContext requestContext, List<ProxyModelFactory> proxyModelFactories, ContentRepository contentRepository, SettingsService settingsService, ServiceConfig serviceConfig) {
-    return new RootContextImpl(site, currentContext, target, requestContext, proxyModelFactories, contentRepository, settingsService, serviceConfig);
-  }
-
-
   @Bean("rootContextFactory")
-  public RootContextFactory rootContextFactory(List<ProxyModelFactory> proxyModelFactories, ContentRepository contentRepository, SettingsService settingsService, ServiceConfig serviceConfig) {
-    return new RootContextFactoryImpl(proxyModelFactories, contentRepository, settingsService, serviceConfig);
+  public RootContextFactory rootContextFactory(BeanFactory beanFactory, List<ProxyModelFactory> proxyModelFactories, ContentRepository contentRepository, SettingsService settingsService, ServiceConfig serviceConfig) {
+    return new RootContextFactoryImpl(beanFactory, proxyModelFactories, contentRepository, settingsService, serviceConfig);
   }
 
 
--- headless-server-lib/src/main/java/com/coremedia/caas/service/repository/RootContextFactoryImpl.java
+++ headless-server-lib/src/main/java/com/coremedia/caas/service/repository/RootContextFactoryImpl.java
@@ -3,16 +3,15 @@ package com.coremedia.caas.service.repository;
 import com.coremedia.blueprint.base.settings.SettingsService;
 import com.coremedia.caas.service.ServiceConfig;
 import com.coremedia.caas.service.request.RequestContext;
+import com.coremedia.caas.service.security.AccessControlViolation;
 import com.coremedia.cap.content.ContentRepository;
 import com.coremedia.cap.multisite.Site;
 
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.BeanFactoryAware;
 
 import java.util.List;
 
-public class RootContextFactoryImpl implements RootContextFactory, BeanFactoryAware {
+public class RootContextFactoryImpl implements RootContextFactory {
 
   private BeanFactory beanFactory;
 
@@ -22,7 +21,8 @@ public class RootContextFactoryImpl implements RootContextFactory, BeanFactoryAw
   private ServiceConfig serviceConfig;
 
 
-  public RootContextFactoryImpl(List<ProxyModelFactory> proxyModelFactories, ContentRepository contentRepository, SettingsService settingsService, ServiceConfig serviceConfig) {
+  public RootContextFactoryImpl(BeanFactory beanFactory, List<ProxyModelFactory> proxyModelFactories, ContentRepository contentRepository, SettingsService settingsService, ServiceConfig serviceConfig) {
+    this.beanFactory = beanFactory;
     this.proxyModelFactories = proxyModelFactories;
     this.contentRepository = contentRepository;
     this.settingsService = settingsService;
@@ -31,13 +31,7 @@ public class RootContextFactoryImpl implements RootContextFactory, BeanFactoryAw
 
 
   @Override
-  public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-    this.beanFactory = beanFactory;
-  }
-
-
-  @Override
-  public RootContext createRootContext(Site site, Object currentContext, Object target, RequestContext requestContext) {
-    return beanFactory.getBean(RootContext.class, site, currentContext, target, requestContext, proxyModelFactories, contentRepository, settingsService, serviceConfig);
+  public RootContext createRootContext(Site site, Object currentContext, Object target, RequestContext requestContext) throws AccessControlViolation {
+    return new RootContextImpl(site, currentContext, target, requestContext, proxyModelFactories, contentRepository, settingsService, serviceConfig, beanFactory);
   }
 }
