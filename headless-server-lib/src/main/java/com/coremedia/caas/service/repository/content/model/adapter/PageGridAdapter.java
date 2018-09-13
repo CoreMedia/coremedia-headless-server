eab77aa2686ea70171d24d77592702196891bdd9
mweikard <mweikard@coremedia.com>
2018-09-13 15:22:52 +0200

    Sort placements by style grid

--- headless-server-lib/src/main/java/com/coremedia/caas/service/repository/content/model/adapter/PageGridAdapter.java
+++ headless-server-lib/src/main/java/com/coremedia/caas/service/repository/content/model/adapter/PageGridAdapter.java
@@ -1,11 +1,17 @@
 package com.coremedia.caas.service.repository.content.model.adapter;
 
 import com.coremedia.blueprint.base.pagegrid.ContentBackedPageGrid;
+import com.coremedia.blueprint.base.pagegrid.ContentBackedPageGridPlacement;
+import com.coremedia.blueprint.base.pagegrid.ContentBackedStyle;
+import com.coremedia.blueprint.base.pagegrid.ContentBackedStyleGrid;
 import com.coremedia.caas.service.repository.RootContext;
+import com.coremedia.cap.content.Content;
 
-import java.util.Collections;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class PageGridAdapter {
 
@@ -19,11 +25,22 @@ public class PageGridAdapter {
   }
 
 
+  @SuppressWarnings("unused")
   public String getCssClassName() {
     return pageGrid.getCssClassName();
   }
 
+  @SuppressWarnings("unused")
   public List<PageGridPlacementAdapter> getPlacements() {
-    return pageGrid.getPlacements().entrySet().stream().map(e -> new PageGridPlacementAdapter(e.getKey(), e.getValue(), rootContext)).collect(Collectors.collectingAndThen(Collectors.toList(), Collections::unmodifiableList));
+    Map<String, ContentBackedPageGridPlacement> placements = pageGrid.getPlacements();
+    ContentBackedStyleGrid styleGrid = pageGrid.getStyleGrid();
+    // fetch placements by style row order
+    return IntStream.range(0, styleGrid.getNumRows())
+            .mapToObj(styleGrid::getRow)
+            .flatMap(Collection::stream)
+            .map(ContentBackedStyle::getSection)
+            .map(Content::getName)
+            .map(e -> new PageGridPlacementAdapter(e, placements.get(e), rootContext))
+            .collect(Collectors.toList());
   }
 }
